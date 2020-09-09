e3e07117b9a1c4141a98392108cdaf1509e3fb5d
= <=>
2020-09-08 22:41:41 -0700

    Add code for simple webview app

---
+++ Cuizine/app/src/main/java/com/example/cuizine/MainActivity.java
@@ -0,0 +1,27 @@
+ package com.example.cuizine;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+ public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.main_view);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://wcfmmp.wcfmdemos.com");
+    }
+}
